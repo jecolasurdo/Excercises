fe1446436c859c9978de90a5454e757913bfcad5
Joe Colasurdo <jecolasurdo@gmail.com>
2014-09-21 13:39:56 -0700

    Stubbed method

--- arraycontainsduplicates/asSpecified-cs/ArrayContainsDuplicates/ArrayContainsDuplicates.cs
+++ arraycontainsduplicates/asSpecified-cs/ArrayContainsDuplicates/ArrayContainsDuplicates.cs
@@ -7,5 +7,23 @@ namespace Katas
 {
     public class ArrayContainsDuplicates
     {
+
+        /// <summary>
+        /// This method determines whether the input array contains any duplicate integers.
+        /// </summary>
+        /// <param name="input">
+        /// An integer array to determine the duplicates of.
+        /// </param>
+        /// <remarks>
+        /// Algorithmic Complexity:
+        /// </remarks>
+        /// <returns>
+        /// Returns true if the integer array contains duplicates.
+        /// Returns false if the integer array does not contain duplicates.
+        /// </returns>
+        public bool ContainsDuplicates(int[] input)
+        {
+            throw new InvalidOperationException();
+        }
     }
 }
--- arraycontainsduplicates/asSpecified-cs/ArrayContainsDuplicatesSpecs/ArrayContainsDuplicatesSpecs.cs
+++ arraycontainsduplicates/asSpecified-cs/ArrayContainsDuplicatesSpecs/ArrayContainsDuplicatesSpecs.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NUnit.Framework;
 
 namespace Katas
 {
-
+    [TestFixture]
     public class ArrayContainsDuplicatesSpecs
     {
 
