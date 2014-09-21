031b3a7784f8046a732ffce9325fe7cebdff63db
Joe Colasurdo <jecolasurdo@gmail.com>
2014-09-21 14:40:34 -0700

    Stubbed ishappyprime function.

---
+++ ishappyprime/asSpecified-cs/IsHappyPrime/HappyPrime.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Katas
+{
+    /// <remarks>
+    /// I've renamed this class from IsHappyPrime to HappyPrimes to be a bit more idiomatic.
+    /// </remarks>>
+    public class HappyPrimes
+    {
+        /// <summary>
+        /// Determines whether the input value is a "happy prime".
+        /// e.g. a prime number that is also a happy number.
+        /// </summary>
+        /// <param name="input">
+        /// An integer to determine the happyness of.
+        /// </param>
+        /// <returns>
+        /// Returns true if the supplied integer is happy.
+        /// Returns false if the supplied integer is not happy.
+        /// </returns>
+	    bool IsHappyPrime(int input) {
+		    throw new NotImplementedException();
+	    }
+    }
+}
