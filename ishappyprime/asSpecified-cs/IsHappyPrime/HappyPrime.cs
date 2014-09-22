b8822ead1e4f45b4d2dbc04497e5bbeec9e8a778
Joe Colasurdo <jecolasurdo@gmail.com>
2014-09-22 15:24:21 -0700

    Simplified HappyPrime class (eliminated interface and injection).
    References #4

--- ishappyprime/asSpecified-cs/IsHappyPrime/HappyPrime.cs
+++ ishappyprime/asSpecified-cs/IsHappyPrime/HappyPrime.cs
@@ -5,38 +5,8 @@ namespace Katas
     /// <remarks>
     /// I've renamed this class from IsHappyPrime to HappyPrimes to be a bit more idiomatic.
     /// </remarks>>
-    public class HappyPrimes : IHappyPrimes
+    public class HappyPrimes
     {
-        public delegate bool IsPrimeDelegate(int value);
-
-        /// <summary>
-        /// A delegate that takes an int and returns a boolean stating whether or not 
-        /// the supplied integer is prime.
-        /// </summary>
-        public IsPrimeDelegate IsPrimeFunction { get; private set; }
-
-        /// <summary>
-        /// Implements the HappyPrimes class with the default prime checking function.
-        /// </summary>
-        public HappyPrimes() {
-            IsPrimeFunction = IsPrime;
-        }
-
-        /// <summary>
-        /// Implemnts the HappyPrimes class with the supplied prime checking delegate.
-        /// </summary>
-        /// <param name="isPrimeDelegate">
-        /// Expects a function that takes an int and returns a boolean.
-        /// </param>
-        /// <remarks>
-        /// This overloaded method is supplied to enable testing of the IsHappyPrime function independant
-        /// of the prime tester. (essentially allowing for the testing of happy numbers, not just primes).
-        /// The inversion of control here also reduces refactoring overhead if better prime algorthms are implemented.
-        /// </remarks>
-        public HappyPrimes(IsPrimeDelegate isPrimeDelegate)
-        {
-            IsPrimeFunction = isPrimeDelegate;
-        }
 
         /// <summary>
         /// Determines whether the input value is a prime number.
@@ -101,7 +71,7 @@ namespace Katas
         /// Returns false if the supplied integer is not happy.
         /// </returns>
 	    public bool IsHappyPrime(int input) {
-            if (IsPrimeFunction(input) == false)
+            if (!IsPrime(input))
             {
                 return false;
             }
