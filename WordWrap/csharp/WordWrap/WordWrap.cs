b07b55dec14dd6beb1368edae5fd62868ba4866a
Joe Colasurdo <jecolasurdo@gmail.com>
2014-09-27 19:09:41 -0700

    Stubbed out wrap method.

--- WordWrap/csharp/WordWrap/WordWrap.cs
+++ WordWrap/csharp/WordWrap/WordWrap.cs
@@ -5,7 +5,21 @@ using System.Text;
 
 namespace Katas
 {
-    public class WordWrap
+    public class Wrapper
     {
+        /// <summary>
+        /// Takes a string of text and inserts linebreak characters according to the supplied column number.
+        /// </summary>
+        /// <param name="textToWrap">The text you want to have wrapped.</param>
+        /// <param name="columnToWrapAt">The column number at which you want to wrap the text.</param>
+        /// <returns>
+        /// Returns the wrapped text.
+        /// </returns>
+        /// <remarks>
+        /// 
+        /// </remarks>
+        public static string WordWrap(string textToWrap, int columnToWrapAt) {
+            throw new NotImplementedException();
+        }
     }
 }
--- WordWrapSpecs/WordWrapSpecs.cs
+++ WordWrapSpecs/WordWrapSpecs.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NUnit.Framework;
 
 namespace Katas
 {
-    public class WordWrapSpecs
+    [TestFixture]
+    public class WrapSpecs
     {
     }
 }
