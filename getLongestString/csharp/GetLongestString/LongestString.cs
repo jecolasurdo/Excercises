540425ad0a787ed5c543c51e19829524120447ff
Joe Colasurdo <joe.colasurdo@mclendons.com>
2014-10-08 16:17:43 -0700

    Stubbed specifications.

--- getLongestString/csharp/GetLongestString/LongestString.cs
+++ getLongestString/csharp/GetLongestString/LongestString.cs
@@ -14,8 +14,14 @@ namespace Katas
         /// <summary>
         /// Given a collection of strings, determines the nTh longest string in the list.
         /// </summary>
-        /// <param name="nTh">The ordinal you are seeking.</param>
-        /// <param name="inputs">The list of strings to search.</param>
+        /// <param name="nTh">The ordinal you are seeking.
+        /// Non-zero based
+        /// Must be a positive integer
+        /// If given number is greater than list length, the longest word in the list is returned.
+        /// </param>
+        /// <param name="inputs">The list of strings to search.
+        /// Null or empty list throws exception.
+        /// </param>
         /// <returns>
         /// Returns the nTh longest string from the supplied list of strings.
         /// </returns>
@@ -27,6 +33,7 @@ namespace Katas
         /// <remarks>
         /// In the original spec the stubbed method is declared with private scope. To ease testing, I exposed the method as public, but this can be changed if desired.
         /// I camal cased the method name here, but that's stylistic and can be changed to conform to other standards.
+        /// I took some liberties with the specifications on this. These could be nailed down more concretely with just a little analysis work.
         /// </remarks>
         public string GetLongestString(int nTh, List<string> inputs) {
             throw new NotImplementedException();
--- getLongestString/csharp/getLongestStringSpecs/LongestStringSpecs.cs
+++ getLongestString/csharp/getLongestStringSpecs/LongestStringSpecs.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using NUnit.Framework;
 
@@ -9,6 +10,57 @@ namespace Katas
     [TestFixture]
     public class LongestStringSpecs
     {
+        [Category("General")]
+        [Test]
+        public void GetLongestString_Normally_ReturnsTheLongestString() {
+            Assert.Inconclusive();
+        }
 
+        [Category("Nth Arg Specs")]
+        [Test]
+        public void GetLongestString_GivenZero_ThrowsInvalidArgumentException() {
+            Assert.Inconclusive();
+        }
+
+        [Category("Nth Arg Specs")]
+        [Test]
+        public void GetLongestString_GivenNegative_ThrowsInvalidArgumentException() {
+            Assert.Inconclusive();
+        }
+
+        [Category("Nth Arg Specs")]
+        [Test]
+        public void GetLongestString_GivenNumberGreaterThanListLength_ReturnsLongestWord() {
+            Assert.Inconclusive();
+        }
+
+        /// <summary>
+        /// Assuming the following list:
+        /// {"TT","FFFFF","SSSSSSS","GGGGG","IIIIII"}
+        /// Both FFFFF and GGGGG are the 3rd longest.
+        /// If nth = 3, the method should return FFFFF, as it is closer to the head of the list.
+        /// 
+        /// An alternative spec could be that the whichever is first alphanumerically is returned, but that
+        /// doesn't seem as in keeping with the idea of exploring the positions within the list. This method
+        /// also avoids the notion that alphanumeic sorting is applicable to list items' domain, which may
+        /// not be a safe assumption.
+        /// </summary>
+        [Category("Nth Arg Specs")]
+        [Test]
+        public void GetLongestString_ItemsWithNLengthInMultiplePositions_ReturnsItemClosestToHeadOfList() {
+            Assert.Inconclusive();
+        }
+
+        [Category("List Specs")]
+        [Test]
+        public void GetLongestString_ListIsEmpty_ThrowsInvalidArgumentException() {
+            Assert.Inconclusive();
+        }
+
+        [Category("List Specs")]
+        [Test]
+        public void GetShortestString_ListIsNull_ThrowsInvalidArgumentException() {
+            Assert.Inconclusive();
+        }
     }
 }
