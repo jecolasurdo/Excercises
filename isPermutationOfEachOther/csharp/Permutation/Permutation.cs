ea57a9fa2f73403d58fcc863a93a420b9c137cdf
Joe Colasurdo <joe.colasurdo@mclendons.com>
2014-10-08 23:20:30 -0700

    Tweaked comments.

--- isPermutationOfEachOther/csharp/Permutation/Permutation.cs
+++ isPermutationOfEachOther/csharp/Permutation/Permutation.cs
@@ -19,9 +19,8 @@ namespace Katas
         /// <remarks>
         /// <para>In the original spec, this method was proposed with private scope. 
         /// I have made the method public to ease testing, but this could be changed.</para>
-        /// <para>This implementation only considers full permutations, 
-        /// therefor if the length of each input string do not match, and the distinct character 
-        /// count of each input string do not match, false is returned.</para>
+        /// <para>This implementation only considers full, bi-directional (commutative) permutations, 
+        /// therefor if the length of each input string do not match, false is returned.</para>
         /// <para>Nulls are regarded as non-permutable, and result in a nullargumentexception.</para>
         /// </remarks>
         public bool IsPermutationOfEachOther(string input1, string input2){
