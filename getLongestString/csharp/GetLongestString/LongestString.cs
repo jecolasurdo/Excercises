054d1c966f128571851f579b3e6e4d371a7922d6
Joe Colasurdo <joe.colasurdo@mclendons.com>
2014-10-08 22:35:22 -0700

    Cleaning up documentation.

--- getLongestString/csharp/GetLongestString/LongestString.cs
+++ getLongestString/csharp/GetLongestString/LongestString.cs
@@ -15,30 +15,29 @@ namespace Katas
         /// <summary>
         /// Given a collection of strings, determines the nTh longest string in the list.
         /// </summary>
-        /// <param name="nTh">The ordinal you are seeking where 1 is the longest word in the list.
-        /// Non-zero based
-        /// Must be a positive integer
-        /// If given number is greater than list length, the shortest word in the list is returned.
-        /// If multiple words in the list are the same length, the word that appears first in the list is returned.
+        /// <param name="nTh">
+        /// The ordinal you are seeking where 1 is the longest word in the list.
+        /// <para>This is not zero based.</para>
+        /// <para>This must be a positive integer.</para>
+        /// <para>If the given number is greater than the list length, the shortest word in the list is returned.</para>
+        /// <para>If multiple words in the list are the same length, the word that appears first in the list is returned.</para>
         /// </param>
         /// <param name="inputs">The list of strings to search.
-        /// Null or empty list throws exception.
+        /// <para>Null or empty list throws exception.</para>
         /// </param>
         /// <returns>
         /// Returns the nTh longest string from the supplied list of strings.
         /// </returns>
         /// <example>
         /// Example: 		
-        ///   {Yuri, Interview, Nordstrom, Cat, Dog, Telephone, AVeryLongString, This code puzzle is easy}
-        ///    nTh = 1 > would return "This code puzzle is easy"
+        /// Given the following list: {Yuri, Interview, Nordstrom, Cat, Dog, Telephone, AVeryLongString, This code puzzle is easy}
+        /// <para>nTh = 1 > would return "This code puzzle is easy"</para>
         /// </example>
         /// <remarks>
-        /// I renamed the method from GetLongestString to GetNthLongestString so that the method's intent is more clear.
-        /// In the original spec the stubbed method is declared with private scope. To ease testing, I exposed the method as public, but this can be changed if desired.
-        /// I camal cased the method name here, but that's stylistic and can be changed to conform to other standards.
-        /// I took some liberties with the specifications on this. These could be nailed down more concretely with just a little analysis work.
-        /// There is more than one way to skin this cat. The method I chose is probably not the most elegant, but is the easiest to understand.
-        /// I generally try to start there then make the method more elegant and efficient as required by its implementation.
+        /// <para>I renamed the method from GetLongestString to GetNthLongestString so that the method's intent is more clear.</para>
+        /// <para>In the original spec the stubbed method is declared with private scope. To ease testing, I exposed the method as public, but this can be changed if desired.</para>
+        /// <para>I camal cased the method name here, but that's stylistic and can be changed to conform to other standards.</para>
+        /// <para>I took some liberties with the specifications on this. These would normally be nailed down more concretely with just some BA work.</para>
         /// </remarks>
         public string GetNthLongestString(int nTh, List<string> inputs) {
             if (nTh <= 0)
@@ -55,6 +54,7 @@ namespace Katas
             {
                 nTh = inputs.Count;
             }
+
             var sortedList = inputs;
             sortedList.Sort((a,b)=>b.Length-a.Length);
             var lengthOfNthItem = sortedList.ElementAt(nTh - 1).Length;
