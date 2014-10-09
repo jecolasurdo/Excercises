46ce0a8ff237f011612971438ad06bd9181d7f5d
Joe Colasurdo <joe.colasurdo@mclendons.com>
2014-10-08 22:48:35 -0700

    Stubbed isPermutationOfEachOther method.

--- isPermutationOfEachOther/csharp/Permutation/Permutation.cs
+++ isPermutationOfEachOther/csharp/Permutation/Permutation.cs
@@ -7,5 +7,21 @@ namespace Katas
 {
     public class Permutation
     {
+        /// <summary>
+        /// Given two input strings, this function determines if two strings are permutations of each other.
+        /// </summary>
+        /// <param name="input1">The first string to consider.</param>
+        /// <param name="input2">The second string to consider.</param>
+        /// <returns>
+        /// <para>Returns true if the strings are permutations of each other.</para>
+        /// <para>Returns false if the strigs are not permutations of each other.</para>
+        /// </returns>
+        /// <remarks>
+        /// In the original spec, this method was proposed with private scope. I have made the metho public to ease testing, but this could be changed.
+        /// </remarks>
+        public bool IsPermutationOfEachOther(string input1, string input2){
+	        throw new NotImplementedException();
+	    }
+	
     }
 }
