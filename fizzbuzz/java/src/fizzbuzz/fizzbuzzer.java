ac3e397755fa3a6a158ef99258784b34f59e4c31
Joe Colasurdo <jecolasurdo@gmail.com>
2014-10-14 23:47:17 -0700

    Created fizzbuzz project

---
+++ fizzbuzz/java/src/fizzbuzz/fizzbuzzer.java
@@ -0,0 +1,11 @@
+package fizzbuzz;
+
+/**
+ *
+ * @author Joe C
+ */
+public class fizzbuzzer {
+    public String FizzBuzz(int input) {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ fizzbuzz/java/test/fizzbuzztests/tests.java
@@ -0,0 +1,16 @@
+package fizzbuzztests;
+
+import fizzbuzz.fizzbuzzer;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Joe C
+ */
+public class tests {
+    @Test
+    public void DummyTest(){
+        fail();
+    }
+}
